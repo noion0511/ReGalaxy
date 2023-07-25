b70c998bbdc15712481fd3ca4a2a4119a33e0871
noion0511 <noion0511@gmail.com>
2023-07-26 00:46:04 +0900

    feat : 커스텀 카메라 기능
    - 안드로이드 4.3버전에 맞춰 작동할 수 있도록 구현
    - 촬영버튼을 누르면 5초를 세고 사진이 찍히는 기능
    - 찍힌 사진이 저장되는 기능
    - 시간초 카운트 다운 기능

--- front/app/src/main/java/com/ssafy/phonesin/CameraActivity.kt
+++ front/app/src/main/java/com/ssafy/phonesin/CameraActivity.kt
@@ -1,85 +1,143 @@
 package com.ssafy.phonesin
 
-import android.Manifest
+import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
-import android.graphics.Bitmap
-import android.os.Build
+import android.hardware.Camera
 import android.os.Bundle
-import android.provider.MediaStore
-import android.widget.Button
-import android.widget.ImageView
+import android.os.CountDownTimer
+import android.os.Environment
+import android.view.SurfaceHolder
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
+import androidx.core.net.toUri
+import androidx.databinding.DataBindingUtil
+import com.ssafy.phonesin.databinding.ActivityCameraBinding
 import java.io.File
 import java.io.FileOutputStream
+import java.text.SimpleDateFormat
+import java.util.*
 
-class CameraActivity : AppCompatActivity() {
-    private val REQUEST_IMAGE_CAPTURE = 1
-    private val REQUIRED_PERMISSIONS = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) arrayOf(Manifest.permission.CAMERA)
-    else arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+class CameraActivity : Activity() {
+
+    private lateinit var binding: ActivityCameraBinding
+
+    private lateinit var camera: Camera
+    private lateinit var surfaceHolder: SurfaceHolder
+    private var isSafeToTakePicture = false
+
+    private val pictureCallback = Camera.PictureCallback { data, _ ->
+        savePicture(data)
+        restartPreview()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_camera)
 
-        val captureButton = findViewById<Button>(R.id.button_capture)
-        captureButton.setOnClickListener {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                if (allPermissionsGranted()) {
-                    dispatchTakePictureIntent()
-                } else {
-                    ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_IMAGE_CAPTURE)
-                }
-            } else {
-                dispatchTakePictureIntent()
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_camera)
+
+        if (checkCameraHardware(this)) {
+            initCamera()
+        } else {
+            // Handle the case where the device doesn't have a camera
+            finish()
+        }
+
+        surfaceHolder = binding.svCamera.holder
+        surfaceHolder.addCallback(surfaceHolderCallback)
+
+        binding.btnTakePicture.setOnClickListener {
+            if (isSafeToTakePicture) {
+                startCountdownAndTakePicture()
             }
         }
     }
 
-    private fun dispatchTakePictureIntent() {
-        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
-            takePictureIntent.resolveActivity(packageManager)?.also {
-                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+    private fun initCamera() {
+        camera = getCameraInstance() ?: return
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+    private fun startCountdownAndTakePicture() {
+        binding.btnTakePicture.isEnabled = false
+
+        object : CountDownTimer(5000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {
+                val secondsRemaining = millisUntilFinished / 1000
+                showToast((secondsRemaining + 1).toString())
+            }
+
+            override fun onFinish() {
+                showToast("사진 촬영 중...")
+                takePicture()
+            }
+        }.start()
+    }
+
+    private fun takePicture() {
+        if (isSafeToTakePicture) {
+            camera.takePicture(null, null, pictureCallback)
+            isSafeToTakePicture = false
+        }
+    }
+
+    private val surfaceHolderCallback = object : SurfaceHolder.Callback {
+        override fun surfaceCreated(holder: SurfaceHolder) {
+            try {
+                camera.setPreviewDisplay(holder)
+                camera.startPreview()
+                isSafeToTakePicture = true
+            } catch (e: Exception) {
+                // Handle the error
             }
         }
+
+        override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
+            // If your camera can support preview size changes, handle it here
+        }
+
+        override fun surfaceDestroyed(holder: SurfaceHolder) {
+            // Release the camera resources when the surface is destroyed
+            camera.release()
+        }
     }
 
-    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
-        ContextCompat.checkSelfPermission(this, it) == PackageManager.PERMISSION_GRANTED
+    private fun restartPreview() {
+        camera.startPreview()
+        isSafeToTakePicture = true
+        binding.btnTakePicture.isEnabled = true
     }
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == REQUEST_IMAGE_CAPTURE) {
-            if (grantResults.isNotEmpty() && grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
-                dispatchTakePictureIntent()
-            } else {
-                Toast.makeText(this, "Permissions not granted by the user.", Toast.LENGTH_SHORT).show()
+    private fun savePicture(data: ByteArray) {
+        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+        val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val imageFile = File(storageDir, "IMG_$timeStamp.jpg")
+
+        try {
+            val fos = FileOutputStream(imageFile)
+            fos.write(data)
+            fos.close()
+
+            // Add the image to the system's gallery (if needed)
+            sendBroadcast(Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, imageFile.toUri()))
+        } catch (e: Exception) {
+            showToast("사진 저장 실패")
+        }
+    }
+
+    companion object {
+        fun checkCameraHardware(context: Context): Boolean {
+            return context.packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA)
+        }
+
+        fun getCameraInstance(): Camera? {
+            return try {
+                Camera.open()
+            } catch (e: Exception) {
+                null
             }
         }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            val imageBitmap = data?.extras?.get("data") as Bitmap
-            findViewById<ImageView>(R.id.imageView).setImageBitmap(imageBitmap)
-            saveImage(imageBitmap)
-        }
-    }
-
-    private fun saveImage(bitmap: Bitmap) {
-        val directory = getExternalFilesDir(null)
-        val file = File(directory, "image.jpg")
-        val fos = FileOutputStream(file)
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos)
-        fos.close()
     }
 }
