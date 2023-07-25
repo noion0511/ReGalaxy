722c975e23672b64ec2177fe520c52727d6be3c9
최희주 <gmlwnchl@naver.com>
2023-07-25 13:56:39 +0900

    chore : start phonesin backend

---
+++ phonesin/src/main/java/com/regalaxy/phonesin/PhonesinApplication.java
@@ -0,0 +1,13 @@
+package com.regalaxy.phonesin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhonesinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhonesinApplication.class, args);
+	}
+
+}
