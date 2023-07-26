837fde55dedee1d782494ea9ba6bce9862109321
박성욱 <tjddnr0110@naver.com>
2023-07-26 15:42:28 +0900

    feat : 임시 member 생성
    - member entity 임시 생성
    - member repository 임시 생성

--- phonesin/src/main/java/com/regalaxy/phonesin/member/model/entity/Member.java
+++ phonesin/src/main/java/com/regalaxy/phonesin/member/model/entity/Member.java
@@ -1,4 +1,25 @@
 package com.regalaxy.phonesin.member.model.entity;
 
+import com.regalaxy.phonesin.donation.model.entity.Donation;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Entity
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Long id;
+
+    private String email;
+    private String member_name;
+    private String password;
+    private String phone_number;
+    private Boolean isCha;
+    private Boolean isBlackList;
+    private Boolean isDelete;
+    private Boolean isManager;
 }
