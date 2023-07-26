6031647b81807a2f38cd355fa83181e38cc1bfc6
장성운 <sungoon0646@gmail.com>
2023-07-26 14:28:03 +0900

    feat : BackDto / Entity 변수 설정과 작성
    close #231

--- phonesin/src/main/java/com/regalaxy/phonesin/back/model/BackDto.java
+++ phonesin/src/main/java/com/regalaxy/phonesin/back/model/BackDto.java
@@ -1,4 +1,33 @@
 package com.regalaxy.phonesin.back.model;
 
+import com.regalaxy.phonesin.back.model.entity.Back;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@ToString
 public class BackDto {
+    private int back_id;
+    private int back_status;
+    private LocalDate back_delivery_date;
+    private LocalDateTime apply_date;
+    private String back_delivery_location_type;
+    private String back_delivery_location;
+
+    public static BackDto toBackDto(Back back) {
+        BackDto backDto = new BackDto();
+        backDto.setBack_id(back.getBack_id());
+        backDto.setBack_status(back.getBack_status());
+        backDto.setBack_delivery_date(back.getBack_delivery_date());
+        backDto.setApply_date(back.getApply_date());
+        backDto.setBack_delivery_location_type(back.getBack_delivery_location_type());
+        backDto.setBack_delivery_location(back.getBack_delivery_location());
+
+        return backDto;
+    }
 }
--- phonesin/src/main/java/com/regalaxy/phonesin/back/model/entity/Back.java
+++ phonesin/src/main/java/com/regalaxy/phonesin/back/model/entity/Back.java
@@ -1,4 +1,47 @@
 package com.regalaxy.phonesin.back.model.entity;
 
+import com.regalaxy.phonesin.back.model.BackDto;
+import com.regalaxy.phonesin.member.model.entity.Member;
+import com.regalaxy.phonesin.rental.model.entity.Rental;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
 public class Back {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int back_id;
+
+    @OneToOne(mappedBy = "back")
+    private List<Rental> rentals = new ArrayList<>();
+
+    @Column(nullable = false)
+    private int back_status;
+
+    @Column(nullable = false)
+    private LocalDate back_delivery_date;
+
+    @Column(nullable = false)
+    private LocalDateTime apply_date;
+
+    private String back_delivery_location_type;
+    private String back_delivery_location;
+    private String back_zipcode;
+    private String review;
+
+    public static Back toBackEntity(BackDto BackDto) {
+        Back back = new Back();
+        back.();
+    }
 }
