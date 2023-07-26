3a3e6965e795c554953f2fccb1e9facbc9bf1fb9
leehandsub <gustjq7027@naver.com>
2023-07-26 14:55:16 +0900

    feat : rental list

---
+++ front/app/src/main/java/com/ssafy/phonesin/ui/mobile/RentalListActivity.kt
@@ -0,0 +1,12 @@
+package com.ssafy.phonesin.ui.mobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ssafy.phonesin.R
+
+class RentalListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rental_list)
+    }
+}
