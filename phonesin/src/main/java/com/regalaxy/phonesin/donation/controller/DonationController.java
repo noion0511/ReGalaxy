c381502c01c1a6d36a6f6503949819472efaae87
박성욱 <tjddnr0110@naver.com>
2023-07-26 22:09:55 +0900

    feat : donation controller 수정  (#S09P12D102-226)
    - 데이터 이동시 entity 로 이동하던 점 dto로 수정

--- phonesin/src/main/java/com/regalaxy/phonesin/donation/controller/DonationController.java
+++ phonesin/src/main/java/com/regalaxy/phonesin/donation/controller/DonationController.java
@@ -1,12 +1,8 @@
 package com.regalaxy.phonesin.donation.controller;
 
-import com.regalaxy.phonesin.donation.model.DonationDto;
-import com.regalaxy.phonesin.donation.model.entity.Donation;
+import com.regalaxy.phonesin.donation.model.dto.DonationDto;
 import com.regalaxy.phonesin.donation.model.service.DonationService;
 import lombok.RequiredArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -36,7 +32,7 @@ public class DonationController {
     public ResponseEntity<Map<String, Object>> donationApply(@RequestBody DonationDto donationDto) {
         Map<String, Object> resultMap = new HashMap<String, Object>();
         try {
-            if (!DonationService.donationApply(donationDto.toEntity())) throw new RuntimeException();
+            if (!DonationService.donationApply(donationDto)) throw new RuntimeException();
             resultMap.put("message", SUCCESS);
             return new ResponseEntity<Map<String, Object>>(resultMap, HttpStatus.OK);
         } catch (Exception e) {
