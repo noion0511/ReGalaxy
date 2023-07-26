aa1cff4acb196a8560b50f7b31896470e5a020b5
leehandsub <gustjq7027@naver.com>
2023-07-26 13:16:59 +0900

    feat : retrofit 추가

--- front/app/src/main/java/com/ssafy/phonesin/ApplicationClass.kt
+++ front/app/src/main/java/com/ssafy/phonesin/ApplicationClass.kt
@@ -1,6 +1,33 @@
 package com.ssafy.phonesin
 
 import android.app.Application
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 class ApplicationClass : Application() {
+    companion object {
+        const val SERVER_URL = ""
+        lateinit var retrofit: Retrofit
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+       /* val okHttpClient = OkHttpClient.Builder()
+            .readTimeout(5000, TimeUnit.MILLISECONDS)
+            .connectTimeout(5000, TimeUnit.MILLISECONDS)
+            .connectTimeout(30, TimeUnit.SECONDS).build()
+        retrofit = Retrofit.Builder()
+            .baseUrl(SERVER_URL)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .client(okHttpClient)
+            .build()*/
+    }
+
+    private val gson: Gson = GsonBuilder()
+        .setLenient()
+        .create()
 }
