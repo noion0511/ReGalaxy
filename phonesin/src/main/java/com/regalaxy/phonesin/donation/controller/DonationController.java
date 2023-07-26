aeb5b1a67742a168b1a84cc1565a85aa3a091aad
박성욱 <tjddnr0110@naver.com>
2023-07-26 15:05:47 +0900

    #S09P12D102-226 feat : Donation Controller update
    - Create 기능
    - Read All 기능

--- phonesin/src/main/java/com/regalaxy/phonesin/donation/controller/DonationController.java
+++ phonesin/src/main/java/com/regalaxy/phonesin/donation/controller/DonationController.java
@@ -1,6 +1,7 @@
 package com.regalaxy.phonesin.donation.controller;
 
 import com.regalaxy.phonesin.donation.model.DonationDto;
+import com.regalaxy.phonesin.donation.model.entity.Donation;
 import com.regalaxy.phonesin.donation.model.service.DonationService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,22 +23,36 @@ import java.util.Map;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/donation")
-@Api(value="휴대폰 기증 API", description = "휴대폰 기증 Controller")
+@Api(value = "휴대폰 기증 API", description = "휴대폰 기증 Controller")
 public class DonationController {
 
-    private final Logger logger = LoggerFactory.getLogger(DonationController.class);
     private static final String SUCCESS = "success";
     private static final String FAIL = "fail";
-    @Autowired
-    private DonationService DonationService;
+
+    private final DonationService DonationService;
 
     @ApiOperation(value = "기기 기부 신청서 작성")
     @GetMapping("/apply")
-    public ResponseEntity<Map<String, Object>> donationApply(DonationDto donationDto){
-        logger.info("기기 기부 신청서 작성");
+    public ResponseEntity<Map<String, Object>> donationApply(@RequestBody DonationDto donationDto) {
         Map<String, Object> resultMap = new HashMap<String, Object>();
         try {
-            if (!DonationService.donationApply(donationDto)) throw new RuntimeException();
+            if (!DonationService.donationApply(donationDto.toEntity())) throw new RuntimeException();
+            resultMap.put("message", SUCCESS);
+            return new ResponseEntity<Map<String, Object>>(resultMap, HttpStatus.OK);
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            resultMap.put("message", FAIL);
+            return new ResponseEntity<Map<String, Object>>(resultMap, HttpStatus.OK);
+        }
+    }
+
+
+    @ApiOperation(value = "기기 기부 신청서 리스트")
+    @GetMapping("/list")
+    public ResponseEntity<Map<String, Object>> donationList() {
+        Map<String, Object> resultMap = new HashMap<String, Object>();
+        try {
+            resultMap.put("donation", DonationService.donationList());
             resultMap.put("message", SUCCESS);
             return new ResponseEntity<Map<String, Object>>(resultMap, HttpStatus.OK);
         } catch (Exception e) {
