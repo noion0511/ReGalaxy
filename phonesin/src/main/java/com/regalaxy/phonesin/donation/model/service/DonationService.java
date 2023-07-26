ab0b2b3c56451ece1db4a37b5306b0fb2f6ce768
박성욱 <tjddnr0110@naver.com>
2023-07-26 15:11:24 +0900

    feat : donation service update (#S09P12D102-226)
    - create 기능 구현
    - read all 기능 구현

--- phonesin/src/main/java/com/regalaxy/phonesin/donation/model/service/DonationService.java
+++ phonesin/src/main/java/com/regalaxy/phonesin/donation/model/service/DonationService.java
@@ -1,22 +1,28 @@
 package com.regalaxy.phonesin.donation.model.service;
-import com.regalaxy.phonesin.donation.model.DonationDto;
+
+import com.regalaxy.phonesin.donation.model.entity.Donation;
 import com.regalaxy.phonesin.donation.model.repository.DonationRepository;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class DonationService {
-    private final Logger logger = LoggerFactory.getLogger(DonationService.class);
-
     private final DonationRepository donationRepository;
 
-    public DonationService(DonationRepository donationRepository) {
-        this.donationRepository = donationRepository;
+    @Transactional
+    public boolean donationApply(Donation donation) {
+        donationRepository.save(donation);
+        return true;
     }
 
-    public boolean donationApply(DonationDto donationDto){
-        donationRepository.save(donationDto);
-        return true;
+    public List<Donation> donationList() {
+        return donationRepository.findAll();
     }
 }
