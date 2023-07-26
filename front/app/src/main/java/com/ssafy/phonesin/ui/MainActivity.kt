8ef4c43c2889c54031e005afe1f9fe36d427ed8a
leehandsub <gustjq7027@naver.com>
2023-07-26 12:50:12 +0900

    feat : navigation 추가

--- front/app/src/main/java/com/ssafy/phonesin/ui/MainActivity.kt
+++ front/app/src/main/java/com/ssafy/phonesin/ui/MainActivity.kt
@@ -2,11 +2,27 @@ package com.ssafy.phonesin.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.ssafy.phonesin.R
+import com.ssafy.phonesin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setNav()
+    }
+
+    private fun setNav() {
+        val navController =
+            supportFragmentManager.findFragmentById(R.id.containerMain)?.findNavController()
+
+        navController?.let {
+            binding.navigationMain.setupWithNavController(it)
+        }
     }
 }
