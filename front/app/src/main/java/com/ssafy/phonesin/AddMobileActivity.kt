fed3d8d7bd558e8066a247ad16a8f3ab5aa7071e
leehandsub <gustjq7027@naver.com>
2023-07-27 09:10:54 +0900

    feat :  기기 대여 신청 ui

---
+++ front/app/src/main/java/com/ssafy/phonesin/AddMobileActivity.kt
@@ -0,0 +1,11 @@
+package com.ssafy.phonesin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddMobileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_moblie)
+    }
+}
