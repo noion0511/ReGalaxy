a26e6babeaa41f07556809070d2184fffb10781a
오희주 <noion0511@gmail.com>
2023-07-27 14:07:07 +0900

    feat : 사진 확인 페이지(S09P12D102-224)
    - 찍은 사진을 확인하는 페이지 추가
    - 4장의 자신을 넘겨서 확인할 수 있는 기능
    - 자신의 현재 페이지를 알 수 있는 인디케이터 추가
    - 사진 뷰페이지 및 어댑터 추가

--- front/app/src/main/java/com/ssafy/phonesin/ui/module/camera/CameraViewerActivity.kt
+++ front/app/src/main/java/com/ssafy/phonesin/ui/module/camera/CameraViewerActivity.kt
@@ -1,41 +1,26 @@
 package com.ssafy.phonesin.ui.module.camera
-
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.graphics.Matrix
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
 import com.ssafy.phonesin.databinding.ActivityCameraViewerBinding
+import me.relex.circleindicator.CircleIndicator
 
 class CameraViewerActivity : AppCompatActivity() {
     private lateinit var binding: ActivityCameraViewerBinding
+    private lateinit var viewPager: ViewPager
+    private lateinit var pagerAdapter: CameraPageAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCameraViewerBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val photoPaths = intent.getStringArrayListExtra("photo_paths") ?: emptyList<String>()
 
+        viewPager = binding.viewPagerPhotoViewer
+        pagerAdapter = CameraPageAdapter(supportFragmentManager, photoPaths)
+        viewPager.adapter = pagerAdapter
 
-        val photoPath = intent.getStringExtra("photo_path")
-
-        if(photoPath != null) {
-            showImage(photoPath)
-        } else {
-//            finish()
-        }
+        val indicator: CircleIndicator = binding.indicator
+        indicator.setViewPager(viewPager)
     }
-
-    private fun showImage(imagePath: String) {
-        val bitmap = BitmapFactory.decodeFile(imagePath)
-
-        val matrix = Matrix().apply {
-            postRotate(90f)
-            postScale(2f, 2f)
-        }
-
-        val rotatedBitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
-
-        binding.photoViewer.setImageBitmap(rotatedBitmap)
-    }
-
 }
