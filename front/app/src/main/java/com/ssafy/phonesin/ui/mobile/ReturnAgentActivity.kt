c795301df307ca6a0f6045803ce3095a46ab84f5
leehandsub <gustjq7027@naver.com>
2023-07-31 14:05:49 +0900

    feat :기기 반납 대리점 ui (#S09P12D102-187)

---
+++ front/app/src/main/java/com/ssafy/phonesin/ui/mobile/ReturnAgentActivity.kt
@@ -0,0 +1,12 @@
+package com.ssafy.phonesin.ui.mobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ssafy.phonesin.R
+
+class ReturnAgentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_return_agent)
+    }
+}
