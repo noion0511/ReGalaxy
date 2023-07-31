fdb533a78e62073d079a6a0ae7211c13fce334f4
leehandsub <gustjq7027@naver.com>
2023-07-31 14:31:22 +0900

    feat : 카카오 api 추가(#S09P12D102-190)

--- front/app/src/main/java/com/ssafy/phonesin/ui/MainActivity.kt
+++ front/app/src/main/java/com/ssafy/phonesin/ui/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.ssafy.phonesin.ui
 
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Base64
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.ssafy.phonesin.R
 import com.ssafy.phonesin.databinding.ActivityMainBinding
+import java.security.MessageDigest
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -13,7 +18,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        try {
+            val information =
+                packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNING_CERTIFICATES)
+            val signatures = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                information.signingInfo.apkContentsSigners
+            } else {
+                TODO("VERSION.SDK_INT < P")
+            }
+            val md = MessageDigest.getInstance("SHA")
+            for (signature in signatures) {
+                val md: MessageDigest
+                md = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                var hashcode = String(Base64.encode(md.digest(), 0))
+                Log.d("hashcode", "" + hashcode)
+            }
+        } catch (e: Exception) {
+            Log.d("hashcode", "에러::" + e.toString())
 
+        }
         setNav()
     }
 
--- front/app/src/main/java/com/ssafy/phonesin/ui/mobile/ReturnAgentActivity.kt
+++ front/app/src/main/java/com/ssafy/phonesin/ui/mobile/ReturnAgentActivity.kt
@@ -1,12 +1,14 @@
 package com.ssafy.phonesin.ui.mobile
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.ssafy.phonesin.R
 
 class ReturnAgentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_return_agent)
+
+
     }
 }
