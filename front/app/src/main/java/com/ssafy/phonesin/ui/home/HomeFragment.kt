b958264fbe705ac389b8303b1f3b002e8db8a127
김은하 <0812keh@naver.com>
2023-08-01 17:19:25 +0900

    feat : 홈 ui resource 추가

--- front/app/src/main/java/com/ssafy/phonesin/ui/home/HomeFragment.kt
+++ front/app/src/main/java/com/ssafy/phonesin/ui/home/HomeFragment.kt
@@ -1,17 +1,23 @@
 package com.ssafy.phonesin.ui.home
 
+import HomeRankAdapter
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewGroup.MarginLayoutParams
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager.widget.ViewPager
-import com.ssafy.phonesin.R
+import com.google.android.material.tabs.TabLayout
 import com.ssafy.phonesin.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
     private lateinit var binding: FragmentHomeBinding
     private lateinit var viewPager: ViewPager
+    private lateinit var indicator: TabLayout
+    private lateinit var rankRecyclerView: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,15 +30,31 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewPager = binding.viewPagerBanner
-        setViewPager(viewPager)
-
-        binding.circleIndicatorBanner.setupWithViewPager(viewPager)
-        viewPager.adapter = HomeViewPagerAdapter(childFragmentManager)
-
+        setBanner()
+        setRank()
     }
 
-    private fun setViewPager(viewPager: ViewPager) {
+    private fun setBanner() {
+        viewPager = binding.viewPagerBanner
+        indicator = binding.indicatorBanner
+
+        indicator.setupWithViewPager(viewPager)
+        viewPager.adapter = HomeViewPagerAdapter(childFragmentManager)
+
+        for (i in 0 until indicator.getTabCount()) {
+            val tab = (indicator.getChildAt(0) as ViewGroup).getChildAt(i)
+            val p = tab.layoutParams as MarginLayoutParams
+            p.setMargins(0, 0, 10, 0)
+            tab.requestLayout()
+        }
+    }
+
+    private fun setRank() {
+        rankRecyclerView = binding.recyclerViewRank
+
+        val rankList = listOf<Int>(1,2,3,4,5)
+        binding.recyclerViewRank.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+        binding.recyclerViewRank.adapter = HomeRankAdapter(rankList)
 
     }
 
