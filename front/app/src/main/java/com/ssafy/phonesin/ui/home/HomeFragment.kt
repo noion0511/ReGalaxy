da7a30a1c19a61c6e24b3940dd6766bc04f2d246
김은하 <0812keh@naver.com>
2023-08-01 17:18:56 +0900

    feat : 홈 화면 구현
    - viewPager를 이용한 홈 배너 구현
    - tabLayout을 이용한 배너 indicator 구현
    - lottie를 이용한 기증 애니메이션 구현
    - recyclerView를 이용한 기증왕 랭킹 구현

--- front/app/src/main/java/com/ssafy/phonesin/ui/home/HomeFragment.kt
+++ front/app/src/main/java/com/ssafy/phonesin/ui/home/HomeFragment.kt
@@ -5,34 +5,38 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.viewpager.widget.ViewPager
 import com.ssafy.phonesin.R
+import com.ssafy.phonesin.databinding.FragmentHomeBinding
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
-
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var viewPager: ViewPager
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewPager = binding.viewPagerBanner
+        setViewPager(viewPager)
+
+        binding.circleIndicatorBanner.setupWithViewPager(viewPager)
+        viewPager.adapter = HomeViewPagerAdapter(childFragmentManager)
+
+    }
+
+    private fun setViewPager(viewPager: ViewPager) {
+
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             HomeFragment().apply {
