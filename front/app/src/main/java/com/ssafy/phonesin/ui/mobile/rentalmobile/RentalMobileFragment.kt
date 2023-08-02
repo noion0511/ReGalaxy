e2bb2355841f91a637bd92b2316757d4cc9732dc
leehandsub <gustjq7027@naver.com>
2023-08-02 16:41:29 +0900

    feat : rental viewmodel추가(#S09P12D102-260)

--- front/app/src/main/java/com/ssafy/phonesin/ui/mobile/rentalmobile/RentalMobileFragment.kt
+++ front/app/src/main/java/com/ssafy/phonesin/ui/mobile/rentalmobile/RentalMobileFragment.kt
@@ -1,15 +1,14 @@
 package com.ssafy.phonesin.ui.mobile.rentalmobile
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.ssafy.phonesin.R
 import com.ssafy.phonesin.databinding.FragmentRentalMoblieBinding
 import com.ssafy.phonesin.ui.MainActivity
+import com.ssafy.phonesin.ui.util.base.BaseFragment
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,11 +20,26 @@ private const val ARG_PARAM2 = "param2"
  * Use the [RentalMobileFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class RentalMobileFragment : Fragment() {
+
+class RentalMobileFragment :
+    BaseFragment<FragmentRentalMoblieBinding>(R.layout.fragment_rental_moblie) {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-    private lateinit var binding: FragmentRentalMoblieBinding
+
+    override fun onCreateBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentRentalMoblieBinding {
+        return FragmentRentalMoblieBinding.inflate(layoutInflater, container, false).apply {
+            lifecycleOwner = viewLifecycleOwner
+        }
+
+    }
+
+    override fun init() {
+        rentalMobileUi()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,27 +51,13 @@ class RentalMobileFragment : Fragment() {
         mainActivity.hideBottomNavi(true)
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        binding = FragmentRentalMoblieBinding.inflate(layoutInflater, container, false)
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        rentalMobileUi()
-    }
-
     private fun rentalMobileUi() {
-        binding.mobileAdd.setOnClickListener {
+        bindingNonNull.mobileAdd.setOnClickListener {
             findNavController().navigate(
                 R.id.action_rentalMobileFragment_to_rentalAddFragment,
             )
         }
-        binding.postRental.setOnClickListener {
+        bindingNonNull.postRental.setOnClickListener {
             findNavController().navigate(
                 R.id.action_rentalMobileFragment_to_rentalPayFragment,
             )
---
+++ front/app/src/main/java/com/ssafy/phonesin/ui/mobile/rentalmobile/RentalViewModel.kt
@@ -0,0 +1,7 @@
+package com.ssafy.phonesin.ui.mobile.rentalmobile
+
+import com.ssafy.phonesin.ui.util.base.BaseViewModel
+
+class RentalViewModel() : BaseViewModel() {
+    
+}
