f2ca2c2db23db31b4f8168c0bb23a9d5109dbcf4
박성욱 <tjddnr0110@naver.com>
2023-08-04 09:54:11 +0900

    feat : donation dto 수정
    - builder 사용하던점 삭제
    - detail response 가 response 상속 받도록 수정
    - swagger 설명 삭제
    - 기부왕 dto 추가
    - 사용하지 않던 dto 삭제

--- phonesin/src/main/java/com/regalaxy/phonesin/donation/model/DonationRequestDto.java
+++ phonesin/src/main/java/com/regalaxy/phonesin/donation/model/DonationRequestDto.java
@@ -12,25 +12,16 @@ import java.time.LocalDateTime;
 @Getter
 @ApiModel(value = "기기 기증서 DTO", description = "기기 기증서 DTO")
 public class DonationRequestDto {
-    @ApiModelProperty(value = "기증서 ID")
-    private long donationId;
+    private long donationId; // 필요없음
 
-    @ApiModelProperty(value = "기증 멤버 ID")
-    private long memberId;
+    private long memberId; // jwt에서 날라옴
 
-    @ApiModelProperty(value = "기증 상태")
     private int donationStatus;
 
-    @ApiModelProperty(value = "기증 날짜")
-    private LocalDateTime donationCreatedAt;
-
-    @ApiModelProperty(value = "기증 배송 신청 날짜")
     private String donationDeliveryDate;
 
-    @ApiModelProperty(value = "기증 배송 장소 타입")
     private String donationDeliveryLocationType;
 
-    @ApiModelProperty(value = "기증 배송 장소")
     private String donationDeliveryLocation;
 
     public Donation toEntity(Member member) throws Exception {
--- phonesin/src/main/java/com/regalaxy/phonesin/donation/model/DonationResponseDto.java
+++ phonesin/src/main/java/com/regalaxy/phonesin/donation/model/DonationResponseDto.java
@@ -1,40 +1,34 @@
 package com.regalaxy.phonesin.donation.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.regalaxy.phonesin.donation.model.entity.Donation;
 import com.regalaxy.phonesin.member.model.entity.Member;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
+import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDateTime;
 
 
 @Getter
-@ApiModel(value = "기기 기증서 List DTO", description = "기기 기증서 List DTO")
 public class DonationResponseDto {
 
-    @ApiModelProperty(value = "기증서 ID")
     private long donationId;
 
-    @ApiModelProperty(value = "기증 멤버 ID")
     private long memberId;
 
-    @ApiModelProperty(value = "기증 상태")
     private int donationStatus;
 
-    @ApiModelProperty(value = "기증 날짜")
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDateTime donationCreatedAt;
 
-    @ApiModelProperty(value = "기증 배송 신청 날짜")
     private String donationDeliveryDate;
 
-    @ApiModelProperty(value = "기증 배송 장소 타입")
     private String donationDeliveryLocationType;
 
-    @ApiModelProperty(value = "기증 배송 장소")
     private String donationDeliveryLocation;
 
-    @Builder
     public DonationResponseDto(Donation donation){
         this.donationId = donation.getDonationId();
         this.memberId = donation.getMember().getMemberId();
