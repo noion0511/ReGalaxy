614a1fee8bc483ff7d2ccdd1498144598841c551
최희주 <gmlwnchl@naver.com>
2023-08-05 13:05:38 +0900

    feat : admin back page list

--- phonesin/src/main/java/com/regalaxy/phonesin/back/model/BackAdminDto.java
+++ phonesin/src/main/java/com/regalaxy/phonesin/back/model/BackAdminDto.java
@@ -1,2 +1,17 @@
-package com.regalaxy.phonesin.back.model.service;public class BackAdminDto {
+package com.regalaxy.phonesin.back.model;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+public class BackAdminDto {
+    private Long backId;
+    private int backStatus;
+    private LocalDateTime createAt;
+    private String review;
 }
