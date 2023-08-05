42c3a3eb8c94794713cb8939cb3f0f8dedc06783
장성운 <sungoon0646@gmail.com>
2023-08-05 22:54:35 +0900

    fix : Security 설정 변경
    - 로그인, 회원가입, 액세스 토큰 발급을 제외하고 모든 api는 로그인을 하지 않으면 접근 불가능하도록 설정 (개발하는 동안 우선 주석처리)

--- phonesin/src/main/java/com/regalaxy/phonesin/member/model/jwt/JwtAuthenticationFilter.java
+++ phonesin/src/main/java/com/regalaxy/phonesin/member/model/jwt/JwtAuthenticationFilter.java
@@ -10,6 +10,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @AllArgsConstructor
@@ -20,12 +21,34 @@ public class JwtAuthenticationFilter extends GenericFilterBean {
     // 토큰이 유효한지 검사
     @Override
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) req;
+        String path = httpRequest.getRequestURI();
 
-        String token = jwtTokenProvider.resolveToken((HttpServletRequest) req);
-        if (token != null && jwtTokenProvider.validateToken(token)) {
-            Authentication auth = jwtTokenProvider.getAuthentication(token);
-            if (auth != null) {
-                SecurityContextHolder.getContext().setAuthentication(auth);
+        if (!path.startsWith("/member/login") && !path.startsWith("/member/signup") && !path.startsWith("/member/token/refresh")) {
+            String token = jwtTokenProvider.resolveToken(httpRequest);
+            if (token != null) {
+                try {
+                    jwtTokenProvider.validateToken(token);
+                } catch (Exception e) {
+                    // 토큰이 유효하지 않을 때
+                    HttpServletResponse httpResponse = (HttpServletResponse) res;
+                    httpResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 401 Unauthorized status
+                    httpResponse.setContentType("application/json; charset=utf8");
+                    httpResponse.getWriter().write("토큰이 만료되었거나 유효하지 않습니다.");
+                    return; // 추가 처리 중지
+                }
+
+                Authentication auth = jwtTokenProvider.getAuthentication(token);
+                if (auth != null) {
+                    SecurityContextHolder.getContext().setAuthentication(auth);
+                }
+            } else {
+                // 토큰이 유효하지 않을 때
+                HttpServletResponse httpResponse = (HttpServletResponse) res;
+                httpResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 401 Unauthorized status
+                httpResponse.setContentType("application/json; charset=utf8");
+                httpResponse.getWriter().write("로그인이 필요한 서비스입니다.");
+                return; // 추가 처리 중지
             }
         }
         filterChain.doFilter(req, res);
