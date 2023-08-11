25127f51f4d6846f74df84d763591dc17f7d822f
최희주 <gmlwnchl@naver.com>
2023-08-11 09:13:34 +0900

    fix : homecam controller

--- phonesin/src/main/java/com/regalaxy/phonesin/module/controller/SignalingController.java
+++ phonesin/src/main/java/com/regalaxy/phonesin/module/controller/SignalingController.java
@@ -1,6 +1,7 @@
 package com.regalaxy.phonesin.module.controller;
 
 import com.regalaxy.phonesin.module.model.SignalDto;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -13,6 +14,7 @@ import java.util.Set;
 
 @Controller
 @CrossOrigin("*")
+@Api(value = "홈캠 기증 API", description = "홈캠 기증 Controller")
 public class SignalingController {
 
     @Autowired
