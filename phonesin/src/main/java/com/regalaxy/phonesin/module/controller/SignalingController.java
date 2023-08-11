2b5a066e25c50b34eacd15e0abd37dda09ac4d2d
최희주 <gmlwnchl@naver.com>
2023-08-11 09:38:37 +0900

    fix : homecam

--- phonesin/src/main/java/com/regalaxy/phonesin/module/controller/SignalingController.java
+++ phonesin/src/main/java/com/regalaxy/phonesin/module/controller/SignalingController.java
@@ -1,28 +1,28 @@
-package com.regalaxy.phonesin.module.controller;
-
-import com.regalaxy.phonesin.module.model.SignalDto;
-import io.swagger.annotations.Api;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestBody;
-
-import java.sql.SQLOutput;
-import java.util.Set;
-
-@Controller
-@CrossOrigin("*")
-@Api(value = "홈캠 기증 API", description = "홈캠 기증 Controller")
-public class SignalingController {
-
-    @Autowired
-    private SimpMessagingTemplate messagingTemplate;
-
-    @MessageMapping("/signal")
-    public void handleSignal(@RequestBody SignalDto signalDto) {
-        System.out.println(signalDto.getContent().toString());
-        messagingTemplate.convertAndSend("/topic/"+signalDto.getRoomId(), signalDto.getContent());
-    }
-}
+//package com.regalaxy.phonesin.module.controller;
+//
+//import com.regalaxy.phonesin.module.model.SignalDto;
+//import io.swagger.annotations.Api;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.messaging.handler.annotation.MessageMapping;
+//import org.springframework.messaging.simp.SimpMessagingTemplate;
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.RequestBody;
+//
+//import java.sql.SQLOutput;
+//import java.util.Set;
+//
+//@Controller
+//@CrossOrigin("*")
+//@Api(value = "홈캠 기증 API", description = "홈캠 기증 Controller")
+//public class SignalingController {
+//
+//    @Autowired
+//    private SimpMessagingTemplate messagingTemplate;
+//
+//    @MessageMapping("/signal")
+//    public void handleSignal(@RequestBody SignalDto signalDto) {
+//        System.out.println(signalDto.getContent().toString());
+//        messagingTemplate.convertAndSend("/topic/"+signalDto.getRoomId(), signalDto.getContent());
+//    }
+//}
