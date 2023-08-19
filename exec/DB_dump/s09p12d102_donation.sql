79385da78215fadef6a41300fc6318c74d60a0ea
박성욱 <tjddnr0110@naver.com>
2023-08-20 01:29:13 +0900

    feat : exec 폴더 생성 및 내용 작성
    - 빌드 배포 작성
    - 외부 서비스 작성

---
+++ exec/DB_dump/s09p12d102_donation.sql
@@ -0,0 +1,49 @@
+-- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
+--
+-- Host: i9d102.p.ssafy.io    Database: s09p12d102
+-- ------------------------------------------------------
+-- Server version	8.0.34
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `donation`
+--
+
+DROP TABLE IF EXISTS `donation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `donation` (
+  `donation_id` int NOT NULL AUTO_INCREMENT,
+  `member_id` int NOT NULL,
+  `donation_status` int DEFAULT NULL,
+  `donation_delivery_date` date DEFAULT NULL,
+  `donation_delivery_location_type` varchar(45) COLLATE utf8mb4_bin DEFAULT NULL,
+  `donation_delivery_location` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL,
+  `created_at` datetime(6) DEFAULT NULL,
+  `updated_at` datetime(6) DEFAULT NULL,
+  PRIMARY KEY (`donation_id`),
+  KEY `fk_donation_User1_idx` (`member_id`),
+  CONSTRAINT `fk_donation_User1` FOREIGN KEY (`member_id`) REFERENCES `member` (`member_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=120 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-08-19 22:58:42
---
+++ exec/README.md
@@ -0,0 +1,86 @@
+# 빌드 및 배포
+
+## Spring Server
+JVM : openjdk:8-jdk   
+server : ubuntu 20.04 LTS (GNU/Linux 5.4.0-1018-aws x86_64)   
+IDE : IntelliJ IDEA 20201.2.4   
+Spring boot : 2.7.14   
+
+프로젝트에서 사용한 라이브러리 버전
+![spring classpath](/exec/resources/classpath.PNG)
+
+application.yml
+
+```
+spring:
+  mail:
+    host: {사용하는 SMTP 서버 주소}
+    port: {사용하는 SMTP 서버 포트번호}
+    username: {사용하는 SMTP 서버 아이디}
+    password: {사용하는 SMTP 서버 앱 비밀번호}
+```
+
+application-db.yml
+
+```
+spring:
+  datasource:
+    driver-class-name: {DB에 맞는 driver}
+    url: {DB URL}
+    username: {계정명}
+    password: {비밀번호}
+```
+
+### VM option
+-Djasypt.encryptor.password=s1s2a3f4y@
+
+#### build 방법
+```
+cd phonesin
+./gradlew clean build -x test
+```
+
+## 주요 계정 및 프로퍼티가 정의된 파일 목록
+
+phonesin/src/main/resources/application.yml
+phonesin/src/main/resources/application-db.yml
+
+
+# 외부 서비스
+
+### 카카오 MAP
+
+Kakao 지도 Android API 는 앱 키 발급 및 키 해시를 등록해야만 사용 가능합니다.
+이를 위해서는 카카오 계정이 필요합니다.
+
+키 해시 등록을 위해서는 아래 과정이 필요합니다.
+1. 카카오 개발자사이트 (https://developers.kakao.com) 접속
+2. 개발자 등록 및 앱 생성
+3. Android 플랫폼 추가: 앱 선택 – [플랫폼] – [Android 플랫폼 등록] – 패키지명(필수) 등록
+4. 키 해시 등록: [Android 플랫폼 등록] – 키 해시를 등록합니다.
+5. 페이지 상단의 [네이티브 앱 키], 등록한 [패키지명], [키 해시]를 사용합니다.
+
+### 구글 메일
+
+폰고지신 프로젝트에서는 SMTP를 사용하기위해 구글을 이용하였습니다.
+
+1. 구글 계정 보안 설정 (https://myaccount.google.com/u/1/security) 접속
+2. 2단계 인증이 되어 있지 않은경우 2단계 인증 설정
+3. Google에 로그인하는 방법 - [2단계 인증] - [앱 비밀번호] 설정
+4. 앱설정 -> 메일, 기타 설정 ->  기타(맞춤 이름) 입력
+5. 사용하는 앱 이름을 입력하고 생성을 누르면 앱 비밀번호가 제공된다.
+
+application.yml 해야하는 설정
+```
+spring:
+  mail:
+    host: stmp.gmail.com
+    port: 587
+    username: {google 이메일}
+    password: {위에서 설정한 앱 비밀번호}
+```
+
+
+
+# 시연 시나리오
+
